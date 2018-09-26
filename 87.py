4f017d271043b443fbcea4e9d3e1b23cee07b3d4
shivaniyads25 <43026095+shivaniyads25@users.noreply.github.com>
2018-09-26 13:54:44 +0530

    Create 87.py

---
+++ 87.py
@@ -0,0 +1,3 @@
+# python-programs
+shivani
+the name of my college is cmr university.it is loacted in bagalur near chagalatti.it was started in the year 2016.it is a school of engineering and technology.the campus is well equiped and has a strength of about 600 students in total.
