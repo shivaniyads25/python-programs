88572e14c0a7db921b09b8c80d6091e4a538cda9
shivaniyads25 <43026095+shivaniyads25@users.noreply.github.com>
2018-09-27 12:39:39 +0530

    Delete 89.py

--- 89.py
+++
@@ -1,8 +0,0 @@
-shivani
-the name of my college is cmr university.it is loacted in bagalur near chagalatti.it was started in the year 2016.it is a school of engineering and technology.the campus is well equiped and has a strength of about 600 students in total.the value of a is
-100
-the value of b is
-300
-the value of a+b is 
-400
-
