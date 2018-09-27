2a89b39f007585331b22d78a91fdb17b61a29346
shivaniyads25 <43026095+shivaniyads25@users.noreply.github.com>
2018-09-27 12:33:16 +0530

    Add files via upload

---
+++ college.py
@@ -0,0 +1,4 @@
+#printing name
+print("shivani")
+#details about college
+print("the name of my college is cmr university.it is loacted in bagalur near chagalatti.it was started in the year 2016.it is a school of engineering and technology.the campus is well equiped and has a strength of about 600 students in total.")
