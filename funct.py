b6964c627eea6eda003b3e75d90cfdb398c3743d
shivaniyads25 <43026095+shivaniyads25@users.noreply.github.com>
2018-09-29 12:30:47 +0530

    Add files via upload

---
+++ funct.py
@@ -0,0 +1,27 @@
+#functions
+a=10
+b=5
+#defining addition
+def add():
+	return a+b
+#defining subtraction 
+def sub():
+	print("subtraction=",a-b)
+	return
+#defining multiplication
+def multiply():
+	print("multiplication=",a*b)
+	return
+#defining division
+def division():
+	print("division=",a/b)
+	return
+#calling add() function
+r=add()
+print("addition=",r)
+#calling sub() function
+sub()
+#calling multiply() function
+multiply()
+#calling dvision() function
+division()
