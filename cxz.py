66a42e1ce2838549bd748349b5b5db2c6f045054
shivaniyads25 <43026095+shivaniyads25@users.noreply.github.com>
2019-07-15 11:54:53 +0530

    Add files via upload

---
+++ cxz.py
@@ -0,0 +1,3 @@
+a={1:'red',2:'blue',3:'green'}
+b={5:'pink',6:'black',7:'white'}
+print(cmp(a,b))
---
+++ vcx.py
@@ -0,0 +1,10 @@
+a={1:'red',2:'blue',3:'green',4:'yellow'}
+for x in a:
+	print(x)
+for i in a:
+	print(a[i])
+a[1]=('pink')
+a[2]=('black')
+a[3]=('white')
+a[4]=('purple')
+print(a)
